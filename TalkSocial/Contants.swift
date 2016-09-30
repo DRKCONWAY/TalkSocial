70f2a2b2e61355678d0ec7bed4983c962af6b29d
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-29 21:43:05 -0500

    major ui overhaul

---
+++ TalkSocial/Contants.swift
@@ -0,0 +1,11 @@
+//
+//  Contants.swift
+//  TalkSocial
+//
+//  Created by D on 9/29/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.00/255.0
