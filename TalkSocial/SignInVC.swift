bbeca004b2b33cc7a91be9bcf3b63cd071d63488
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-10-02 10:36:19 -0500

    email auth

--- TalkSocial/SignInVC.swift
+++ TalkSocial/SignInVC.swift
@@ -16,7 +16,12 @@ class SignInVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SignInVC.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
+    
+
 
     
 // This function is how you authenticate with Facebook
@@ -26,16 +31,49 @@ class SignInVC: UIViewController {
         facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
             if error != nil {
                 
-                print("Derek: Unable to authenticate with Facebook! - \(error)")
+                print("Derek: Unable to authenticate with Facebook. Doh! - \(error)")
             } else if result?.isCancelled == true {
                 print("Derek: User cancelled FB authentication!")
             } else {
                 print("Derek: Successfully authenticated with Facebook!")
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuthentication(credential)
             }
         }
         
     }
-
+    
+    // Authentication with Firebase
+    func firebaseAuthentication(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                print("Derek: Unable to authenticate with Firebase. Doh! - \(error)")
+            } else {
+                print("Derek: Successfully authenticated with Firebase!")
+            }
+        })
+    }
+    
+    // Dismiss the keyboard when background is tapped
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
 
