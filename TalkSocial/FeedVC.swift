cae7c1e7aaf79c2782f9c5848aabeddcd545ff2d
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-10-03 22:24:54 -0500

    finished up the feed UI and tableview

--- TalkSocial/FeedVC.swift
+++ TalkSocial/FeedVC.swift
@@ -11,13 +11,28 @@ import SwiftKeychainWrapper
 import FirebaseAuth
 
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return (tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell)!
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    
     @IBAction func SignOutTapped(_ sender: AnyObject) {
         let keychainResult = KeychainWrapper.defaultKeychainWrapper().removeObjectForKey(KEY_UID)
         print("Derek: ID was successfully removed from Keychain!! \(keychainResult)")
---
+++ TalkSocial/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  TalkSocial
+//
+//  Created by D on 10/3/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+}
